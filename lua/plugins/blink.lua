432a10960eae137113b350d06bc7d0c7dd9e9574
Aimad Ahsan <ahsanaimad@gmail.com>
2025-02-01 22:58:49 +0530

    Fix issue with blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -25,7 +25,7 @@ return {
             nerd_font_variant = 'mono'
         },
         completion = {
-            list = { selection = 'manual' },
+            list = { selection = { preselect = false, auto_insert = false } },
             documentation = {
                 auto_show = true,
                 auto_show_delay_ms = 500,
