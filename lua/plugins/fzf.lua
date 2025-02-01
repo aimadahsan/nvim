78bf08debbd63d5ff95b22b08b63bccdf285bd44
Aimad Ahsan <ahsanaimad@gmail.com>
2025-02-01 23:59:55 +0530

    Update: More of FzfLua

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -5,6 +5,7 @@ return {
     keys = {
         { "<leader>ff", "<cmd>FzfLua files<CR>", desc = "[f]zf: find [f]iles" },
         { "<leader>fg", "<cmd>FzfLua grep_project<CR>", desc = "[f]zf: [g]rep files" },
+        { "<leader>fb", "<cmd>FzfLua buffers<CR>", desc = "[f]zf: [b]uffers" },
         { "<leader>fh", "<cmd>FzfLua helptags<CR>", desc = "[f]zf: find [h]elp" },
         { "<leader>ft", "<cmd>TodoFzfLua<CR>", desc = "[f]zf: find [t]odos" },
     }
--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -41,19 +41,19 @@ return {
                 vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
 
                 opts.desc = "LSP: [g]o to [d]efinitions"
-                vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+                vim.keymap.set('n', 'gd', '<cmd>FzfLua lsp_definitions jump_to_single_result=true ignore_current_line=true<cr>', opts)
 
                 opts.desc = "LSP: [g]o to [D]eclaration"
-                vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+                vim.keymap.set('n', 'gD', '<cmd>FzfLua lsp_declarations jump_to_single_result=true ignore_current_line=true<cr>', opts)
 
                 opts.desc = "LSP: [g]o to [i]mplementation"
-                vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+                vim.keymap.set('n', 'gi', '<cmd>FzfLua lsp_implementations jump_to_single_result=true ignore_current_line=true<cr>', opts)
 
                 opts.desc = "LSP: [g]o to type definiti[o]n"
-                vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+                vim.keymap.set('n', 'go', '<cmd>FzfLua lsp_typedefs jump_to_single_result=true ignore_current_line=true<cr>', opts)
 
                 opts.desc = "LSP: [g]o to [r]eferences"
-                vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+                vim.keymap.set('n', 'gr', '<cmd>FzfLua lsp_references jump_to_single_result=true ignore_current_line=true<cr>', opts)
 
                 opts.desc = "LSP: [g]o to [s]ignature"
                 vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
@@ -62,7 +62,7 @@ return {
                 vim.keymap.set('n', '<leader>cr', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
 
                 opts.desc = "LSP: Select [c]ode [a]ction"
-                vim.keymap.set('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+                vim.keymap.set('n', '<leader>ca', '<cmd>FzfLua lsp_code_actions', opts)
             end,
         })
     end
