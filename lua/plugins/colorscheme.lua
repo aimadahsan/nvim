0324387138953c0932b7bdeb6d6128d6cc8a56b8
Aimad Ahsan <ahsanaimad@gmail.com>
2025-02-02 01:44:42 +0530

    Change to catppuccin color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,14 @@
 return {
-    "folke/tokyonight.nvim",
+    "catppuccin/nvim",
     lazy = false,
     priority = 1000,
+    name = "catppuccin",
     config = function()
         -- Default options:
-        require("tokyonight").setup {
-            style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+        require("catppuccin").setup {
+            flavour = "mocha",
         }
         vim.o.background = "dark"
-        vim.cmd.colorscheme('tokyonight')
+        vim.cmd.colorscheme("catppuccin")
     end,
 }
