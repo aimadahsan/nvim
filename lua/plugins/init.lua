d062a0765319fad0eede2f0c6dd7fa8f7b95dcfe
Aimad Ahsan <ahsanaimad@gmail.com>
2025-02-23 14:15:11 +0000

    remove config body

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,1 +1,1 @@
-return { "aimadahsan/nvim", priority = 10000, lazy = false, opts = {}, cond = true, version = "*", config = function() module.setup({}) end}
+return { "aimadahsan/nvim", priority = 10000, lazy = false, opts = {}, cond = true, version = "*", config = function() end}
