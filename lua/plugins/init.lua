baa55846eb0aa1eb37330f19a5ecfe789fe5a641
Aimad Ahsan <ahsanaimad@gmail.com>
2025-02-23 17:10:24 +0530

    Update init.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,1 +1,1 @@
-return { "aimadahsan/nvim", priority = 10000, lazy = false, opts = {}, cond = true, version = "*", config = function() require("nvim").setup() end}
+return { "aimadahsan/nvim", priority = 10000, lazy = false, opts = {}, cond = true, version = "*", config = function() print("config") end}
